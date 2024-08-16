8f6278effe908d8486d3f52c284b9b5667225ce1
Moortu <kris.elsinga@protonmail.com>
2024-08-16 22:40:48 +0200

    empty cilium

--- cilium-manifest.tf
+++
@@ -1,8 +0,0 @@
-# kustomize cilium manifests
-resource "local_file" "cilium_kustomization" {
-  filename = "${path.module}/fluxcd/cilium/base/kustomization.yaml"
-  content  = templatefile("${path.module}/fluxcd/cilium/base/kustomization.yaml.tpl", {
-    cilium_version = var.cilium_version
-  })
-}
-
