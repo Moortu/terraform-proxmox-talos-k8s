77ac5c1c8f9da0fa7575e4c248706c7cbfc80a5f
kris <kris@wecitylnl>
2025-11-12 18:39:38 +0100

    added back the proxmox nodes structure

--- stacks/production/02-vms/variables.tf
+++ stacks/production/02-vms/variables.tf
@@ -20,9 +20,49 @@ variable "proxmox_api_token_secret" {
 }
 
 variable "proxmox_nodes" {
-  description = "Proxmox nodes configuration"
-  type        = map(any)
+  description = "Proxmox servers on which the talos cluster will be deployed"
+  type = map(object({
+    control_planes = optional(list(object({
+      name = optional(string)
+      node_labels = optional(map(string), {})
+      taints_enabled = optional(bool, true)
+      network_bridge = optional(string, "vmbr0")
+      mac_address = optional(string)
+      cpu_type = optional(string, "host")
+      cpu_sockets = optional(number, 1)
+      cpu_cores = optional(number, 2)
+      memory = optional(number, 8)
+      boot_disk_size = optional(number, 0)
+      boot_disk_storage_pool = string
+      data_disks = optional(list(object({
+        device_name = string
+        mount_point = string
+        size = number
+        storage_pool = optional(string, "")
+      })), [])
+    })))
+
+    workers = optional(list(object({
+      name = optional(string)
+      node_labels = optional(map(string), {})
+      network_bridge = optional(string, "vmbr0")
+      mac_address = optional(string)
+      cpu_type = optional(string, "host")
+      cpu_sockets = optional(number, 1)
+      cpu_cores = optional(number, 2)
+      memory = optional(number, 8)
+      boot_disk_size = optional(number, 0)
+      boot_disk_storage_pool = string
+      data_disks = optional(list(object({
+        device_name = string
+        mount_point = string
+        size = number
+        storage_pool = optional(string, "")
+      })), [])
+    })))
+  }))
 }
+
 
 variable "talos_iso_destination_filename" {
   type    = string
