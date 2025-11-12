e872ce7589ff31da54ec633d3a578465bb8a8656
kris <kris@wecitylnl>
2025-11-12 18:33:36 +0100

    added missing variables

--- stacks/production/02-vms/variables.tf
+++ stacks/production/02-vms/variables.tf
@@ -1,3 +1,24 @@
+variable "proxmox_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "proxmox_user" {
+  description = "Proxmox API user"
+  type        = string
+}
+
+variable "proxmox_api_token_id" {
+  description = "Proxmox API token ID"
+  type        = string
+}
+
+variable "proxmox_api_token_secret" {
+  description = "Proxmox API token secret"
+  type        = string
+  sensitive   = true
+}
+
 variable "proxmox_nodes" {
   description = "Proxmox nodes configuration"
   type        = map(any)
